6f6e0a0e4da73358ba8a164ce42c56b8adee5248
danren-lys <lys981201@qq.com>
2026-05-30 13:46:57 +0800

    后端404修复，前端音色更新，声音模型更新

--- controller/playground.go
+++ controller/playground.go
@@ -12,6 +12,48 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func PlaygroundAudio(c *gin.Context) {
+	var newAPIError *types.NewAPIError
+
+	defer func() {
+		if newAPIError != nil {
+			c.JSON(newAPIError.StatusCode, gin.H{
+				"error": newAPIError.ToOpenAIError(),
+			})
+		}
+	}()
+
+	useAccessToken := c.GetBool("use_access_token")
+	if useAccessToken {
+		newAPIError = types.NewError(errors.New("暂不支持使用 access token"), types.ErrorCodeAccessDenied, types.ErrOptionWithSkipRetry())
+		return
+	}
+
+	relayInfo, err := relaycommon.GenRelayInfo(c, types.RelayFormatOpenAIAudio, nil, nil)
+	if err != nil {
+		newAPIError = types.NewError(err, types.ErrorCodeInvalidRequest, types.ErrOptionWithSkipRetry())
+		return
+	}
+
+	userId := c.GetInt("id")
+
+	userCache, err := model.GetUserCache(userId)
+	if err != nil {
+		newAPIError = types.NewError(err, types.ErrorCodeQueryDataError, types.ErrOptionWithSkipRetry())
+		return
+	}
+	userCache.WriteContext(c)
+
+	tempToken := &model.Token{
+		UserId: userId,
+		Name:   fmt.Sprintf("playground-%s", relayInfo.UsingGroup),
+		Group:  relayInfo.UsingGroup,
+	}
+	_ = middleware.SetupContextForToken(c, tempToken)
+
+	Relay(c, types.RelayFormatOpenAIAudio)
+}
+
 func Playground(c *gin.Context) {
 	var newAPIError *types.NewAPIError
 
